f7ce64a85ee7c79212862bc95498cc9fe2a9f2dd
shadwar123 <shadwarnayyar@gmail.com>
2024-04-29 02:43:16 +0530

    Chrome Extension

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FakeNews from './FakeNews';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FakeNews/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FakeNews.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css'; // Import your CSS file
+
+const FakeNews = () => {
+  const [content, setContent] = useState('');
+  const [prediction, setPrediction] = useState('');
+
+  const handleInputChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/fake_news_prediction', {
+        content: content
+      });
+      setPrediction(response.data.prediction);
+    } catch (error) {
+      console.error('Error predicting:', error);
+    }
+  };
+
+  return (
+    <div className="fake-news-container">
+      <h1>Fake News Prediction</h1>
+      <div className="textarea-container">
+        <label htmlFor="content">Content:</label>
+        <textarea id="content" value={content} onChange={handleInputChange} />
+      </div>
+      <button onClick={handleSubmit}>Predict</button>
+      {prediction && (
+        <div className="prediction-container">
+          <h2>Prediction:</h2>
+          <p className="prediction-text">{prediction}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FakeNews;
